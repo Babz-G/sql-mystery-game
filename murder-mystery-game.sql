daa45eb15f25d451c9d8026fe227bb5e8948397b
Babz-G <barbaramgaynor@gmail.com>
2026-02-10 17:12:58 -0500

    complete first prompt

--- murder-mystery-game.sql
+++ murder-mystery-game.sql
@@ -9,4 +9,18 @@
 -- writing a comment like this
 
 -- selecting all columns from the crime scene report
+
+-- A crime has taken place and the detective needs your help.
+-- The detective gave you the crime scene report, but you somehow lost it.
+-- You vaguely remember that the crime was a ​murder​ that occurred sometime on 
+-- Jan.15, 2018​ and that it took place in ​SQL City​. Start by retrieving the
+-- corresponding crime scene report from the police department’s database.
+
 SELECT * FROM crime_scene_report;
+
+SELECT *  FROM crime_scene_report WHERE date = '20180115'
+
+SELECT *  FROM crime_scene_report
+WHERE date = '20180115'
+AND city = 'SQL City'
+AND type = 'murder'
