c9a62999bfeef443d93b3c6449a7901728d0f63b
Babz-G <barbaramgaynor@gmail.com>
2026-02-13 15:21:21 -0500

    solve mystery

--- murder-mystery-game.sql
+++ murder-mystery-game.sql
@@ -82,3 +82,86 @@ AND membership_status = 'gold';
 --name: Jeremy Bowers
 --membership_start_date: 20160101
 --membership_status: gold
+
+SELECT * FROM get_fit_now_check_in
+WHERE membership_id LIKE '48Z%'
+AND check_in_date = 20180109;
+
+-- membership_id: 48Z7A
+-- check_in_date: 20180109
+-- check_in_time: 1600
+-- check_out_time: 1730
+
+-- membership_id: 48Z55
+-- check_in_date: 20180109
+-- check_in_time: 1530
+-- check_out_time: 1700
+
+SELECT * FROM drivers_license
+WHERE plate_number LIKE '%H42W%';
+
+--id: 183779
+--age: 21
+--height: 65
+--eye_color: blue
+--hair_color: blonde
+---gender: female
+--plate_number: H42W0X
+--car_make: Toyota
+--car_model: Prius
+
+--id: 423327
+--age: 30
+--height: 70
+--eye_color: brown
+--hair_color: brown
+---gender: male
+--plate_number: 0H42W2
+--car_make: Chevrolet
+--car_model: Spark LS
+
+--id: 664760
+--age: 21
+--height: 71
+--eye_color: black
+--hair_color: black
+---gender: male
+--plate_number: 4H42WR
+--car_make: Nissan
+--car_model: Altima
+
+
+SELECT * FROM person
+WHERE id = 28819
+OR id = 67318;
+
+--id: 28819
+--name: Joe Germuska
+--license_id: 173289
+--address_number: 111
+--address_street_name: 	Fisk Rd
+--ssn: 138909730
+
+
+--id: 67318
+--name: Jeremy Bowers
+--license_id: 423327
+--address_number: 530
+--address_street_name: Washington Pl, Apt 3A
+--ssn: 871539279
+
+INSERT INTO solution VALUES (1, 'Jeremy Bowers');
+SELECT value FROM solution;
+
+-- value: Congrats, you found the murderer! But wait, there's more...
+-- If you think you're up for a challenge, try querying the interview
+-- transcript of the murderer to find the real villain behind this crime.
+-- If you feel especially confident in your SQL skills, try to complete
+-- this final step with no more than 2 queries. Use this same INSERT statement
+-- with your new suspect to check your answer.
+
+-- Jeremy Bowers with license_id 423327
+	
+--✨ I feel like I made it a lot more difficult than it should have been. ✨
+--✨ I will revisit this weekend most likely. ✨
+--✨ Handing in, as is, for the sake of the assignment. ✨
